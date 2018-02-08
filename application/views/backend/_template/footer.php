f20b6105af5792cbef0a7bd92c5b72169a41f864
Emmanuel Campait <ecampait@gmail.com>
2018-02-08 10:33:58 +0100

    [UPD] Core AdminLTE 2.4.2

--- application/views/backend/_template/footer.php
+++ application/views/backend/_template/footer.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div class="pull-right hidden-xs">
-	<b>Version</b> 2.4.0
+	<b>Version</b> 2.4.2
 </div>
 
 <strong>Copyright &copy; 2014-<?php echo date('Y'); ?> <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights reserved.
