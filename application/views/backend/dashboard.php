55624ace287aec3e2903b946f750ecb1a68ae7e9
Emmanuel Campait <ecampait@gmail.com>
2018-02-14 09:00:15 +0100

    [ADD] Display type environment of publication

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -1,6 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-?>
 
+switch (ENVIRONMENT) {
+	case 'development':
+		$color = 'danger';
+		break;
+	case 'testing':
+		$color = 'warning';
+		break;
+	case 'production':
+		$color = 'success';
+		break;
+	default:
+		$color = 'primary';
+		break;
+}
 
+echo ui_callout('alert-' . $color, 'Type of publication', 'The server is in <span class="text-uppercase"><strong>' . ENVIRONMENT . '</strong></span> mode.');
